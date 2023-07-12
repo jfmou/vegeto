5d9acb494e3b0d48dd7cd46a19153d25b665d9e3
Jean-François Mougnot <jmougnot@bouyguestelecom.fr>
2023-07-12 16:32:04 +0200

    Fix .liquid extension renaming

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,14 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/assets")
 
-    eleventyConfig.setLiquidOptions({
-      dynamicPartials: false,
-    });
-    
-    // Return your Object options:
-    return {
-      dir: {
-        input: "src",
-        output: "dist"
-      }
+  eleventyConfig.setLiquidOptions({
+    dynamicPartials: false,
+  });
+  
+  // Return your Object options:
+  return {
+    dir: {
+      input: "src",
+      output: "dist"
     }
-  };
+  }
+};
