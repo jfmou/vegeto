84105477ec5936462e648f958311eb8b3364a47e
Jean-François Mougnot <jmougnot@bouyguestelecom.fr>
2023-07-13 10:39:20 +0200

    Introduction of tina search conf

--- tina/config.ts
+++ tina/config.ts
@@ -149,4 +149,12 @@ export default defineConfig({
       },
     ],
   },
+  search: {
+    tina: {
+      indexerToken: '<Your Search Token>',
+      stopwordLanguages: ['fra']
+    },
+    indexBatchSize: 100,
+    maxSearchIndexFieldLength: 100
+  },
 });
